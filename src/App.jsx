b67d6c5742fbdffb7fcd222ff808d70c25f16d24
AlanMcIvor <alan.mcivor27@gmail.com>
2024-02-20 15:41:56 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import Home from './pages/shared/Home'
+import Login from './pages/shared/Login'
+import Register from './pages/shared/Register'
+// import { Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <ul className="flex gap-10 my-5 mx-5">
+            <li className="text-2xl">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="text-2xl">
+              <Link to="/login">Login</Link>
+            </li>
+            <li className="text-2xl">
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
---
+++ src/pages/shared/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+    return(
+        <>
+            <h1 className="text-3xl font-bold underline">Register</h1>
+        </>
+    )
+}
+
+export default Register;
