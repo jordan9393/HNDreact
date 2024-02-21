c7434ce002a9b98e980682e0ad9d8b270c4db6c0
jordan9393 <jaycuff93@gmail.com>
2024-02-21 14:54:51 +0000

    21/2

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,48 @@ import './App.css'
 import Home from './pages/shared/Home'
 import Login from './pages/shared/Login'
 import Register from './pages/shared/Register'
-import { FiActivity } from "react-icons/fi";
 // import { Router } from 'react-router-dom'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-
 
 function App() {
 
   return (
     <Router>
+      <div className="mainNav">
+
       <div>
-        <nav className="flex items-center mx-5">
-        <FiActivity size = '32' />
-          <ul className="flex gap-10 my-5 mx-5">
-            <li className="text-2xl">
+        <nav>
+          <ul className="">
+            <li className="text-2xl display-inline">
               <Link to="/">Home</Link>
             </li>
             <li className="text-2xl">
               <Link to="/login">Login</Link>
             </li>
-            <li className="text-2xl">
+            <li className="text-2xl ">
               <Link to="/register">Register</Link>
             </li>
           </ul>
         </nav>
+     
+    
 
         <hr />
+     
 
+
+      </div>
+      <div className="mainNav1">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </div>
+      </div>
     </Router>
+    
+    
   )
 }
 
